82f79af701f138d7b842f4372f445b2bc4e7aa1b
Thomas Sammons <t.sammons@hotmail.com>
2021-03-28 15:26:03 +0100

    pass in user as argument

--- docker_install.sh
+++ docker_install.sh
@@ -1,9 +1,7 @@
  #!/bin/bash
  
  curl -fsSL https://get.docker.com -o get-docker.sh
- sudo ./get-docker.sh
+ chmod +x get-docker.sh
+ ./get-docker.sh
 
-read -p "What is yout current user? "
-username="$REPLY"
-
- sudo usermod -aG docker $username
+ sudo usermod -aG docker $1
